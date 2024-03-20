b96aa44d451430b685d71752282e3b5087ced02e
mcook <mcook0775@gmail.com>
2024-03-20 19:19:35 -0400

    first version done

---
+++ bash/deploy-simple-loadbalancer.sh
@@ -0,0 +1,58 @@
+#!bin/bash
+
+# This script sets up a simple Linux vm scale set behind a load balancer translating port 80 to 5000
+# The purpose is to allow the API to run without root permissions for security purposes and ease of deployment
+
+RG='api-demo-rg'
+LOC='eastus'
+LB='api-demo-lb'
+VNET='api-demo-vnet'
+SN='LBBackendPoolSubnet'
+NSG='api-demo-nsg'
+LB_IP='api-demo-lb-ip'
+LB_FE_IP='api-demo-lb-frontend-ip'
+VMSS='api-demo-scaleset'
+
+az group create --name $RG --location $LOC
+
+az network vnet create --name $VNET \
+    --resource-group $RG \
+    --location $LOC \
+    --address-prefixes 10.0.0.0/16 \
+    --subnet-name $SN \
+    --subnet-prefixes 10.0.1.0/24
+
+# Create an NSG with port 5000 inbound opened
+az network nsg create --name $NSG \
+    --resource-group $RG \
+    --location $LOC
+
+az network nsg rule create --name AllowPort5000 \
+    --nsg-name $NSG \
+    --resource-group $RG \
+    --priority 100 \
+    --direction Inbound \
+    --access Allow \
+    --protocl Tcp
+
+az vmss create --name $VMSS \
+    --resource-group $RG \
+    --image Ubuntu2204 \
+    --vm-sku Standard_DC1s_v2 \
+    --vnet-name $VNET \
+    --subnet $SN \
+    --nsg $NSG \
+    --admin-username michael \
+    --generate-ssh-keys
+
+az network lb rule create --name "$LB-HTTP-rule" \
+    --resource-group $RG \
+    --lb-name $LB \
+    --backend-pool-name "$LB-BackendPool" \
+    --backend-port 5000 \
+    --frontend-ip-name $LB_FE_IP \
+    --frontend-port 80 \
+    --protocol tcp
+
+
+# create VM scale set and add to lb backend pool
